786892313eb96a6ebd6f585bb68bd16839aff6db
Richmond Watkins <richmondwatkins@mac.com>
2014-07-17 15:55:18 -0500

    initial

---
+++ lib/month.rb
@@ -0,0 +1,30 @@
+class Month
+  MONTHS = [nil, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  def initialize(month, year)
+    @month = month
+    @year = year
+  end
+
+  def header
+    "#{name} #{@year}".center(20).rstrip
+  end
+
+  def name
+    MONTHS[@month]
+  end
+
+  def to_s
+    output = header
+    output << "\nSu Mo Tu We Th Fr Sa\n"
+    output << <<EOS
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+EOS
+    output
+  end
+end
